b20c81580fa548aee092422909c81e8ae1f796fe
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2025-08-05 16:50:57 -0600

    actualizacion deproyecto
    error en el header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
-import Header from "./components/header/header";
 import PaginaJuegos from './components/paginaJuegos/PaginaJuegos'
 import PaginaVarias from './components/paginaVarias/PaginaVarias';
 import PaginaWeb from './components/paginaWeb/PaginaWeb'
 import Contacto from "./components/contacto/Contacto";
 import AcercaDe from "./components/acercaDe/AcercaDe";
+import Header from "./components/header/Header";
 
 
 function App() {
